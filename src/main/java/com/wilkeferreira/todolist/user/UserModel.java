b102c15680d495aac1070f8447081c949fd24640
wilker818 <wilkerferreira818@gmail.com>
2023-10-11 04:26:29 -0300

    feat(userModel/pom): adicionado lombok para auto criacao do getter e setter

--- src/main/java/com/wilkeferreira/todolist/user/UserModel.java
+++ src/main/java/com/wilkeferreira/todolist/user/UserModel.java
@@ -1,33 +1,25 @@
 
 package com.wilkeferreira.todolist.user;
 
+import lombok.Data;
+
+/**
+ * cria getter e setter automaticos
+ * https://projectlombok.org/setup/maven
+ ********************************
+ * import lombok.Data;
+ * import lombok.Getter;
+ * import lombok.Setter;
+ ********************************
+ * @Data -> cria getter e setter
+ * @Getter -> cria getter
+ * @Setter -> cria setter
+ ********************************
+ */
+@Data
 public class UserModel {
     private String username;
     private String name;
     private String password;
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 
 }
