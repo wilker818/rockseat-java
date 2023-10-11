4b7e4a407c736d56d779dac7e3c6cab2a97f7db9
wilker818 <wilkerferreira818@gmail.com>
2023-10-11 05:22:37 -0300

    feat(user):  configura mensagens de erro

--- src/main/java/com/wilkeferreira/todolist/user/UserController.java
+++ src/main/java/com/wilkeferreira/todolist/user/UserController.java
@@ -1,6 +1,9 @@
 package com.wilkeferreira.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,9 +32,17 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if (user != null) {
+            // Mensagem de erro
+            // Status code
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario existente");
+        }
+
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
--- src/main/java/com/wilkeferreira/todolist/user/UserModel.java
+++ src/main/java/com/wilkeferreira/todolist/user/UserModel.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -32,6 +33,7 @@ public class UserModel {
     @GeneratedValue(generator = "UUID")
     private UUID id;
 
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
