3b85499f726127e8bfe53daae86b6c547d7f7a81
wilker818 <wilkerferreira818@gmail.com>
2023-10-13 04:56:04 -0300

    feat(task): criado alteracao das tasks

--- src/main/java/com/wilkeferreira/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/wilkeferreira/todolist/filter/FilterTaskAuth.java
@@ -26,7 +26,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
         var servletPath = request.getServletPath();
 
-        if (servletPath.equals("/tasks/")) {
+        if (servletPath.startsWith("/tasks/")) {
             // Pegar a authorization (usuario e senha)
             var authorization = request.getHeader("Authorization");
             var authEncoded = authorization.substring("Basic".length()).trim();
